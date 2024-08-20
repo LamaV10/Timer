5870a0ca300ea8d6fc24f98a67b9c70d9580b8d5
LamaV10 <lama.customs@gmail.com>
2024-08-20 09:51:08 +0200

    Updated requirements.txt. Now you have to press enter twice to stop the music. At least the program doesn't stop because of an error.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import time
 import math
 import pygame
+import pyautogui as auto
 from pygame import mixer
 
 pygame.init()
 pygame.mixer.init()
 
 
-# Timer for the Lapcount-collision
 timer = float(input("Minuten: "))
 timer = timer * 60
 
 time.sleep(timer)
+auto.write("1")
 
-# music import/play
-mixer.music.load("Music/Meat-Grinder.mp3")
-mixer.music.set_volume(0.5)
-mixer.music.play()
-
-bruno = int(input("Press Enter to stop!"))
+run = True 
+while run:
+    # music import/play
+    mixer.music.load("Music/Meat-Grinder.mp3")
+    mixer.music.set_volume(0.5)
+    mixer.music.play(-1)
+    
+    
+    stop = int(input(auto.write("1")))
+    if stop == 1:
+        run = False 
