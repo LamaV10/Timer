8b3058309cb6f082f7edc878ab2a43f2d0991348
LamaV10 <lama.customs@gmail.com>
2024-10-02 17:44:52 +0200

    Different Timer solution. Before = time.sleep(). Now it's handeld with the unix time difference. This adds the possibility to print the time left in between.

--- Source/main.py
+++ Source/main.py
@@ -1,25 +1,32 @@
 import time
 import math
 import pygame
-
 import music
 from pygame import mixer
 
 pygame.init()
-# pygame.mixer.init()
 
 stop = 0
 
-timer = float(input("Minuten: "))
-timer = timer * 60
+#amount of time is being set
+delay = float(input("Minuten: "))
+delay = delay * 60
+unix_time_diff = time.time() + delay
+current_time = time.time()
 
-time.sleep(timer)
 
 run = True 
 while run:
     # music import/play
-    music.music()    
-    stop = int(input("Type 1 and press enter to stop the timer: "))
-        
+    current_time = time.time()
+
+    if current_time > unix_time_diff:
+        music.music()
+
+    if current_time > unix_time_diff:
+        stop = int(input("Type 1 and press enter to stop the timer: "))
+
     if stop == 1:
         run = False 
+    
+    print(unix_time_diff - current_time)
--- Source/music.py
+++ Source/music.py
@@ -4,6 +4,6 @@ from pygame import mixer
 pygame.init()
 
 def music():
-    mixer.music.load("Music/It-was-a-good-day.mp3")
+    mixer.music.load("Music/Flashing-Lights.mp3")
     mixer.music.set_volume(0.5)
     mixer.music.play(-1)
