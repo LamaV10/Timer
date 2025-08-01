aed3e3bcd214e4097baa4e8b7258a32f314f3f5e
LamaV10 <lama.customs@gmail.com>
2025-08-01 15:09:54 +0200

    Seperate music.cpp file added for easier acces with modified setup.sh script.

---
+++ setup.sh
@@ -0,0 +1,72 @@
+echo "What do you want to setup?"
+echo "Wich Song is going to be played (1) or path of the Timer-folder in timer.sh (2):"
+read modify
+
+
+if [ $modify -eq 1 ]; then
+  echo "Please enter the location of your music file: "
+  read musicLocation
+
+  cd src/
+echo "// #include "timer.cpp"
+#include <ctime>
+#include <chrono>
+#include <iostream>
+#include <unistd.h>
+#include <mpg123.h>
+#include <ao/ao.h>
+
+void audioInit(){
+    mpg123_init();
+    ao_initialize();
+}
+
+void playAudio(){
+    mpg123_handle *mh = mpg123_new(NULL, NULL);
+    mpg123_open(mh, "$musicLocation");
+
+    // set up audio output
+    long rate; // Declare rate
+    int channels; // Declare channels
+    int encoding; // Declare encoding
+    mpg123_getformat(mh, &rate, &channels, &encoding); // Get format
+
+    // Set up the audio format structure
+    ao_sample_format format;
+    format.bits = 16; // Set bits per sample (usually 16 for PCM)
+    format.channels = channels; // Set number of channels
+    format.rate = rate; // Set sample rate
+    format.byte_format = AO_FMT_NATIVE; // Set byte format (native)
+    
+    int driver = ao_default_driver_id();
+    ao_device *device = ao_open_live(driver, &format, NULL); // Pass the format structure
+
+    // Start playing the file
+    unsigned char *buffer;
+    size_t buffer_size = 4096; // Set buffer size
+    buffer = (unsigned char *)malloc(buffer_size); // Allocate buffer
+    size_t done;
+    
+    // Read and decode the audio
+     while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK) { // Check for MPG123_OK
+	    ao_play(device, (char *)buffer, done); // Use done directly
+    }
+    // Clean up
+    free(buffer); // Free allocated buffer
+    ao_close(device);
+    mpg123_delete(mh);
+    mpg123_exit();
+    ao_shutdown();
+}
+" > music.cpp
+fi
+
+
+if [ $modify -eq 2 ]; then
+  path="$(pwd)" 
+
+  echo "cd $path/src
+python main.py" > timer.sh
+
+  echo "The path was setup. You can now copy this script into your /bin directory for easier acces!"
+fi
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,3 +1,4 @@
+#include "music.h"
 #include <ctime>
 #include <chrono>
 #include <iostream>
@@ -5,48 +6,8 @@
 #include <mpg123.h>
 #include <ao/ao.h>
 
-void audioInit(){
-    mpg123_init();
-    ao_initialize();
-}
-
-void playAudio(){
-    mpg123_handle *mh = mpg123_new(NULL, NULL);
-    mpg123_open(mh, "/home/lama/Music/Alt-Rap/Larry June, 2 Chainz, The Alchemist - Life Is Beautiful.mp3");
-
-    // set up audio output
-    long rate; // Declare rate
-    int channels; // Declare channels
-    int encoding; // Declare encoding
-    mpg123_getformat(mh, &rate, &channels, &encoding); // Get format
-
-    // Set up the audio format structure
-    ao_sample_format format;
-    format.bits = 16; // Set bits per sample (usually 16 for PCM)
-    format.channels = channels; // Set number of channels
-    format.rate = rate; // Set sample rate
-    format.byte_format = AO_FMT_NATIVE; // Set byte format (native)
-    
-    int driver = ao_default_driver_id();
-    ao_device *device = ao_open_live(driver, &format, NULL); // Pass the format structure
-
-    // Start playing the file
-    unsigned char *buffer;
-    size_t buffer_size = 4096; // Set buffer size
-    buffer = (unsigned char *)malloc(buffer_size); // Allocate buffer
-    size_t done;
-    
-    // Read and decode the audio
-     while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK) { // Check for MPG123_OK
-	    ao_play(device, (char *)buffer, done); // Use done directly
-    }
-    // Clean up
-    free(buffer); // Free allocated buffer
-    ao_close(device);
-    mpg123_delete(mh);
-    mpg123_exit();
-    ao_shutdown();
-}
+void audioInit();
+void playAudio();
 
 using namespace std;
 int main(){
