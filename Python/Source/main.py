e4b145ea0f46198de561e5d64de6cb90aa04bf96
LamaV10 <lama.customs@gmail.com>
2025-08-02 12:02:35 +0200

    Modified python script -> now uses 0% cpu like the c++ version but still 10x the memory.

--- Python/Source/main.py
+++ Python/Source/main.py
@@ -3,7 +3,6 @@ import math
 import sys
 import pygame
 import music
-from pygame import mixer
 
 pygame.init()
 
@@ -22,37 +21,41 @@ fullMinutes = math.trunc(userDelay)
 # From minutes into seconds.
 delay = userDelay * 60
 # Unix time + the delay the user wants.
-unix_time_wanted = time.time() + delay 
+unix_time_wanted = time.time() + delay
 
-run = True 
+run = True
 while run:
     # current unix time
     current_time = time.time()
 
-    # Leftover time in seconds between unix time and the desiered user time. 
+    # Leftover time in seconds between unix time and the desiered user time.
     leftoverTime = unix_time_wanted - current_time
-    # Minutes that are left over rounded down to have no decimel places! 
+    # Minutes that are left over rounded down to have no decimel places!
     printMinutes = math.trunc(leftoverTime / 60)
-    # Minutes rounded down & multiplied by 60 to get the seconds from the full minutes. This is being subtracted from the total seconds to get the leftover seconds from the current minute.  
-    secondsFromMinutes = (math.trunc(leftoverTime)- (fullMinutes * 60))
+    # Minutes rounded down & multiplied by 60 to get the seconds from the full minutes.
+    # This is being subtracted from the total seconds to get the leftover seconds from the current minute.
+    secondsFromMinutes = (math.trunc(leftoverTime) - (fullMinutes * 60))
 
     if current_time >= unix_time_wanted:
         music.music()
         stop = int(input("Type 1 and press enter to stop the timer: "))
-   
-    # Prints seconds if the there are no more full minutes left. 
+
+    # Prints seconds if the there are no more full minutes left.
     if leftoverTime <= 60:
         print(round(leftoverTime, 1), "s")
         delete_last_line()
 
     # Prints minutes and seconds (min : sec) when there are still full minutes left.
     elif leftoverTime > 60:
-        print((printMinutes),":", (secondsFromMinutes), "min",)
+        print((printMinutes), ":", (secondsFromMinutes), "min", )
         delete_last_line()
 
     # When a full minute is up this fullMinutes variable is being subtracted by 1. This is important for the secondsFromMinutes calculation!
     if secondsFromMinutes < 0:
-        fullMinutes -= 1 
+        fullMinutes -= 1
 
     if stop == 1:
-        run = False 
+        run = False
+    time.sleep(0.1)
+
+
--- Python/Source/music.py
+++ Python/Source/music.py
@@ -5,8 +5,7 @@ pygame.init()
 def music():
       # music import/play
       # change your path to your music here:
-      mixer.music.load('/home/lama/Music/Alt-Rap/Larry June, The Alchemist - 60 Days.mp3')
-      
+      mixer.music.load('/home/marcel/Music/Alt-Rap/Larry June, The Alchemist - 60 Days.mp3')
 
       mixer.music.set_volume(0.5)
       mixer.music.play(-1)
